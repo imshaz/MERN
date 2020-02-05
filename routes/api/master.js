4382c9ea05dc47fdb2f8a6aa6bd7339e140759dd
shaz <imshazbangash@gmail.com>
2020-02-06 02:04:53 +0500

    new file master.js is added to master branch

---
+++ routes/api/master.js
@@ -0,0 +1,25 @@
+// New file created on master branch 
+// for Testing git commands only
+
+// TODO: dELETE THIS FILE
+
+const express = require('express');
+
+const router = express.Router();
+
+//@route   GET api/user/id
+//@desc    get user by id
+//@access   public    
+router.get('/:id', (req, res) => {
+    res.send("get single user by id")
+})
+
+//@route   get api/users
+//@desc    get list of users 
+//@access   public    
+router.get('/', (req, res) => {
+    res.send("get users")
+})
+
+
+module.exports = router; 
