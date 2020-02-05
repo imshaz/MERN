db4b7ab6a358459be25adc1b3fcaae5b57d94603
shaz <imshazbangash@gmail.com>
2020-02-06 02:00:12 +0500

    added new route to users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,18 @@
-const express = require('express'); 
+const express = require('express');
 
-const router = express.Router(); 
+const router = express.Router();
 
-//@route   GET api/user
-//@desc    Test Route
+//@route   GET api/user/id
+//@desc    get user by id
 //@access   public    
-router.get('/', (req, res)=>{
+router.get('/:id', (req, res) => {
+    res.send("get single user by id")
+})
+
+//@route   get api/users
+//@desc    get list of users 
+//@access   public    
+router.get('/', (req, res) => {
     res.send("get users")
 })
 
